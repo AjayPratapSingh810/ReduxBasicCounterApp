582e73e7b2e4005ed8e71bded563321b137e1765
unknown <pratapaj810@gmail.com>
2024-03-19 12:25:09 +0530

    second commit

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,7 +2,7 @@ import Display from "./Display"
 import { addNumber, subNumber } from "../store/number"
 import { useDispatch } from "react-redux";
 export default function Home() {
-    const dispatch = useDispatch();
+    const dispatch = useDispatch(); git
     return <>
         <h1>Counter with redux</h1>
         <button onClick={() => dispatch(addNumber())}>Add</button>
